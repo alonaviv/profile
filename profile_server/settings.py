f82fb47b6455198bab555ea91741c88aac7c31ba
Alon Aviv <alona@infinidat.com>
2020-12-23 00:38:01 +0200

    Frontend for next meeting

--- profile_server/settings.py
+++ profile_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^i34c0ernf6maes&z7d=5uk))vq8i^ot!p!qzk@c5-9rx+i%*@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.111', 'localhost']
+ALLOWED_HOSTS = ['192.168.0.111', 'localhost', '127.0.0.1']
 
 
 # Application definition
