414a3825c92830afc10a9cfa71a531dbf76c0581
Alon Aviv <alona@infinidat.com>
2021-01-08 14:44:49 +0200

    Display edit information for user in admin area

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from .models import TeacherUser
 from django.contrib.auth.admin import UserAdmin
 
+
 class CustomUserAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {
+            'fields': ('is_homeroom_teacher', 'house', 'teacher_object'),
+        }),
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
         (None, {
             'fields': ('is_homeroom_teacher', 'house', 'teacher_object'),
         }),
